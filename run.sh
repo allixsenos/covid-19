d383dbf6e02f890eb19f6eaab4f677bfeadd58d9
Luka Kladaric <luka@hey.com>
2021-03-19 13:26:26 +0100

    lol my shell fu is weeeeak

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 set -e
 cp support/header.csv data/croatia.csv
 curl "https://www.koronavirus.hr/json/?action=podaci" > data/raw
-cat data/raw | jq -er '. | map([(.Datum|split(" ")[0]), (.SlucajeviHrvatska|tostring), (.IzlijeceniHrvatska|tostring), (.UmrliHrvatska|tostring)] | join(",")) | join("\n")' | tac | awk '{print ++a "," $0}' | tac >> data/croatia.csv
+cat data/raw | jq -er '. | map([(.Datum|split(" ")[0]), (.SlucajeviHrvatska|tostring), (.IzlijeceniHrvatska|tostring), (.UmrliHrvatska|tostring)] | join(",")) | join("\n")' > data/raw2
+# give jq a chance to fail the job before clobbering the data file
+tac data/raw2 | awk '{print ++a "," $0}' | tac >> data/croatia.csv
