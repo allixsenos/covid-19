48c71e9a82f9217e24674e0ff74feb967fb610b1
Luka Kladaric <luka@hey.com>
2021-03-19 13:19:09 +0100

    manually update again. make script fail on error.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 cp support/header.csv data/croatia.csv
 curl "https://www.koronavirus.hr/json/?action=podaci" > data/raw
 cat data/raw | jq -r '. | map([(.Datum|split(" ")[0]), (.SlucajeviHrvatska|tostring), (.IzlijeceniHrvatska|tostring), (.UmrliHrvatska|tostring)] | join(",")) | join("\n")' | tac | awk '{print ++a "," $0}' | tac >> data/croatia.csv
