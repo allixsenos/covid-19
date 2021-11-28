b98c78f3407e4f3840a20827cb48f9bc77367ef6
Luka Kladaric <luka@hey.com>
2021-11-28 20:01:06 +0100

    fix for unsorted data

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ set -e
 cp support/header.csv data/croatia.csv
 curl "https://www.koronavirus.hr/json/?action=podaci" > data/raw
 cat data/raw | jq -er '. | map([(.Datum|split(" ")[0]), (.SlucajeviHrvatska|tostring), (.IzlijeceniHrvatska|tostring), (.UmrliHrvatska|tostring)] | join(",")) | join("\n")' > data/raw2
+sort -r data/raw2 > data/raw2.sorted
 # give jq a chance to fail the job before clobbering the data file
-tac data/raw2 | awk '{print ++a "," $0}' | tac >> data/croatia.csv
+tac data/raw2.sorted | awk '{print ++a "," $0}' | tac >> data/croatia.csv
